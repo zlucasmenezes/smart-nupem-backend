1a0f5dcdef8ce6e1d1d2e8c0d653716de4727bf9
Lucas <zlucasmenezes@gmail.com>
2020-08-22 00:42:14 -0300

    change get devices route

--- src/app/routes/board.routes.ts
+++ src/app/routes/board.routes.ts
@@ -6,7 +6,7 @@ import timeSeriesController from '../controllers/ts.controller';
 const routes = Router();
 
 routes.post('/auth', boardController.auth);
-routes.get('/device', boardGuard.isAuthenticated, boardController.getDevices);
+routes.get('/devices', boardGuard.isAuthenticated, boardController.getDevices);
 routes.post('/ts/:sensorId', boardGuard.isAuthenticated, timeSeriesController.insert);
 
 export default routes;
